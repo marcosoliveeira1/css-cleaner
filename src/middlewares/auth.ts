7dde0ff9b769c2f4f150a738ab784d01b3810729
Marcos Oliveira <marcosoliveeira1@users.noreply.github.com>
2025-08-22 19:01:29 -0300

    refactor: log key if auth is incorrect

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,7 +3,10 @@ import type { Context, Next } from "hono";
 export const authMiddleware = async (c: Context<{ Bindings: { API_KEY: string } }>, next: Next) => {
     const auth = c.req.header("Authorization");
 
+    console.log("Verificando autenticação:", auth);
+
     if (!auth || auth !== `Bearer ${c.env.API_KEY}`) {
+        console.log("API_KEY:", c.env.API_KEY);
         return c.json({ error: "Não autorizado" }, 401);
     }
     await next();
